00f0c10f7b4890f8b3481dcea6d1ea6e33a6721d
gabrielparim-stack <gabriel.pari.m@tecsup.edu.pe>
2025-08-29 22:48:26 -0500

    Responsable: agregado método calculateAverage en StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,10 +1,20 @@
 public class StudentController {
     public static void main(String[] args) {
         System.out.println("Hello from StudentController");
+
+        // Probando el nuevo método
+        showStudent("Gabriel");
+        double promedio = calculateAverage(15, 18, 20);
+        System.out.println("El promedio calculado es: " + promedio);
     }
 
-    // Nuevo método de ejemplo
+    // Método existente
     public static void showStudent(String name) {
         System.out.println("Mostrando información del estudiante: " + name);
+    }
+
+    // Nuevo método agregado por el Responsable
+    public static double calculateAverage(int nota1, int nota2, int nota3) {
+        return (nota1 + nota2 + nota3) / 3.0;
     }
 }
